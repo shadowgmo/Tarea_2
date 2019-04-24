74a0785893575849a546175c46fd9b3c71117595
shadowgmo <lgcastro15@gmail.com>
2019-04-24 14:10:51 -0600

    Add files via upload

---
+++ Tarea 2.py
@@ -0,0 +1,29 @@
+#Ejercicio 1
+
+
+my_list =  [5, 6, 10, 13, 3, 4]
+
+sum = my_list[0] + my_list[1] + my_list[2] + my_list[3] + my_list[4] + my_list[5]
+
+promedio = sum / 6
+
+print(promedio)
+
+#Ejercicio 2
+
+
+todos = [
+
+[177,145,167,190,140,150,180,130], # grupo 1
+
+[165,176,145,189,170,189,159,190], # grupo 2
+
+[145,136,178,200,123,145,145,134], # grupo 3
+
+[201,110,187,175,156,165,156,135] # grupo 4
+
+]
+max(todos)
+
+print("los numeros mayores son : ", max(todos))
+
